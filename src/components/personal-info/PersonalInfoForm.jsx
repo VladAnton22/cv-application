0ce2d395177e0651846d7d5aa226d9b9e8deb9d5
Vlad Anton <182820291+VladAnton22@users.noreply.github.com>
2025-10-04 21:30:32 +0100

    Made some small changes, just changed padding and favicon

--- src/components/personal-info/PersonalInfoForm.jsx
+++ src/components/personal-info/PersonalInfoForm.jsx
@@ -8,7 +8,7 @@ export default function PersonalInfoForm({
   address,
 }) {
   return (
-    <div class="p-3 border-2 mb-6 rounded-md border-gray-300">
+    <div class="p-3 pb-6 border-2 mb-6 rounded-md border-gray-300">
       <h2 className="text-2xl font-semibold mb-2 text-center">Personal Info</h2>
       <form className="max-w-sm mx-auto flex flex-col gap-4 font-sans">
         <InputDiv
